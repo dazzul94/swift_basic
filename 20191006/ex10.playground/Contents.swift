1dcdbfac18f5ae22ef9a3bf6dc307aa934a8c6fa
503 <a503-22@503ui-iMac.local>
2019-10-06 10:45:07 +0900

    2019-10-06 10:45

---
+++ 20191006/ex10.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+// 값 타입과 참조 타입
+// value type 과 reference type
+// value type: 변수(메모리공간) 안에 실제 데이터가 저장되어 있음. 값 복사 간편.
+// reference type: 실제 데이터가 들어있는 메모리공간의 주소값을 저장함. 값 복사시 주소값을 전달함.
