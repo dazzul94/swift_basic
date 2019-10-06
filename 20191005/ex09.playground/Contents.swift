3b015404d78fbaef9f060b149941c84de8336be0
503 <a503-22@503ui-iMac.local>
2019-10-06 10:33:56 +0900

    2019-10-06 10:33

--- 20191005/ex09.playground/Contents.swift
+++ 20191005/ex09.playground/Contents.swift
@@ -44,3 +44,60 @@ for (name, money) in persons {
 for i in 0...30 where i % 2 == 0 {
     print(i)
 }
+
+
+// while문
+// 초기화
+// while 조건문 - 소괄호는 사라짐
+// {
+//      수행문..
+//      증감문
+// }
+
+var index = 0
+while index < 10 {
+    print(index)
+    index += 1  // ++연산자가 사라짐
+}
+
+// break: 반복문 탈출
+// continue: 반복문 귀환. 다시한번 루프를 돌린다. 처음으로 돌린다.
+
+// 무한반복문
+while true {
+    print(index)
+    index += 1
+    if index > 100 {
+        break
+    }
+}
+
+for i in 1...100 {
+    if i % 2 == 0 { // 2의 배수임. 짝수임.
+        continue    // 반복문의 처음으로 돌아간다.
+    }
+    print(i)
+}
+
+// repeat-while 문
+// repeat-while과 while의 차이점: 조건이 앞에 있냐 뒤에있냐 차이
+// 적어도 한번은 실행된다.
+
+// 초기화
+// repeat {
+//      수행문...
+//      증감문
+// } while 조건문
+
+var index2 = 0
+repeat {
+    print(index2)
+    index2 += 1
+} while index2 < 100
+
+// 이중 for문: for문 안에 for문 - 구구단 출력
+for i in 2...9 {
+    for j in 1...9 {
+        print("\(i) * \(j) = \(i * j)") // 인터폴레이션
+    }
+}
