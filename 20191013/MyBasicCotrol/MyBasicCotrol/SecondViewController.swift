c7ef434ace04ade9d2f614b6c1f1081cbc7081f7
dazzul94 <dazzul94@gmail.com>
2019-10-13 15:29:58 +0900

    2019-10-16 15:29

--- 20191013/MyBasicCotrol/MyBasicCotrol/SecondViewController.swift
+++ 20191013/MyBasicCotrol/MyBasicCotrol/SecondViewController.swift
@@ -12,6 +12,8 @@ import UIKit
 class SecondViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var textfieldData: UITextField!
     @IBOutlet weak var textviewData: UITextView!
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,9 +27,38 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
     }
     
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        print("textFieldshouldreturn 입력됨.")
+        print("textFieldShouldEndEditing")
         return true
         // return false로 하면 이벤트가 무시된다. -> 처리를 하지않는다.
     }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("textFieldShouldReturn")
+        return true
+    }
     
+    @IBAction func onSwitchChanged(_ sender: UISwitch) {
+        print("스위치 변경됨.", sender.isOn)
+        // 켜지면 true, 꺼지면 false
+    }
+    
+    @IBAction func onSegmentChanged(_ sender: UISegmentedControl) {
+        print("세그먼트 변경됨.", sender.selectedSegmentIndex)
+        switch sender.selectedSegmentIndex {
+        case 0:
+            progressView.progress = 0.3
+            indicator.startAnimating()
+        case 1:
+            progressView.progress = 0.6
+        case 2:
+            progressView.progress = 0.9
+            indicator.stopAnimating()
+        default:
+            progressView.progress = 0
+        }
+    }
+    @IBAction func onSliderChanged(_ sender: UISlider) {
+        print("슬라이더 변경됨.", sender.value)    // 0.0 ~ 1.0
+        progressView.progress = sender.value    // slider를 바꾸면 progress도 바뀐다.
+    }
 }
