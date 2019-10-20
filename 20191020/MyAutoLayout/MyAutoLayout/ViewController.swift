ca6baa3e864f850272bd561d3cc477ab14643fd6
dazzul94 <dazzul94@gmail.com>
2019-10-20 11:17:43 +0900

    2019-10-20 11:17

---
+++ 20191020/MyAutoLayout/MyAutoLayout/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyAutoLayout
+//
+//  Created by 503 on 2019/10/20.
+//  Copyright © 2019 dasol. All rights reserved.
+//
+
+import UIKit
+
+// 화면 레이아웃 배치 방법 2가지
+// 1. 오토 리사이징(Auto Resizing): 이전방법(간단, 비추천)
+// 2. 오토 레이아웃(Auto Layout): 현재 주로 사용(추천)
+
+// + 1가지(SwiftUI, XCode11, MacOS카탈리나 10.15 새로 발표)
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
