e1e82721072b65c6acdaddfd4d3bfe93185db240
dazzul94 <dazzul94@gmail.com>
2019-11-02 15:12:03 +0900

    2019-11-01 15:12

---
+++ 20191101/MyFirebase/MyFirebase/UploadViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UploadViewController.swift
+//  MyFirebase
+//
+//  Created by 503 on 2019/11/02.
+//  Copyright © 2019 dasol. All rights reserved.
+//
+
+import UIKit
+
+class UploadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
